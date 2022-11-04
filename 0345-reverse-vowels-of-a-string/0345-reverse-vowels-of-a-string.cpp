0d732ee97a14da13def20a459d2225a99576d569
Rajat Kumar Kansal <70747298+RajatKumarK@users.noreply.github.com>
2022-11-04 07:13:45 +0530

    Time: 5 ms (93.51%), Space: 7.8 MB (61.66%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -2,19 +2,42 @@ class Solution {
 public:
     string reverseVowels(string s) {
         int n = s.length();
-        stack<char> stk;
-        for(int i=0;i<n;i++){
+        int r = n-1;
+        int l = 0;
+        bool fl=false;
+        bool fr=false;
+        int i;
+        while(l<r){
+            i = l;
             if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
-              stk.push(s[i]);  
+              fl = true;
             }
+            i = r;
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+              fr = true;
+            }
+            if(fl&&fr){
+                char t = s[r];
+                s[r] = s[l];
+                s[l] = t;
+                r--;
+                l++;
+                fr=false;
+                fl=false;
+            }
+            else if(fl){
+                r--;
+            }
+            else if(fr){
+                l++;
+            }
+            else{
+                r--;
+                l++;
+            }
+            
         }
         
-        for(int i=0;i<n;i++){
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
-                s[i] = stk.top();
-                stk.pop();
-            }
-        }
         
         return s;
     }
